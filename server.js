2ee00bfbf86de51c4102f04b23de98f71a32c4ff
Pavel <pavel.ps2014@yandex.ru>
2021-11-26 07:42:52 +0300

    Развернул приложение

---
+++ server.js
@@ -0,0 +1,18 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+const app = express();
+app.use(requireHTTPS);
+app.use(express.static('./dist/my-todos.json'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/my-todos.json/'}
+  );
+  });
+  app.listen(process.env.PORT || 8080);
