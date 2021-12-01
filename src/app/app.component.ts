bc08210bcf5c9c8e6a041fac3d57ed29c9d3ea78
Pavel <pavel.ps2014@yandex.ru>
2021-12-01 10:28:34 +0300

    ДОБАВИЛ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { Todo } from './Todo';
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-todos';
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
+
+  
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, ReactiveFormsModule, MatCardModule,MatIconModule,HttpClientModule],
+  declarations: [ AppComponent, HeaderComponent, FormComponent],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  
+
+
+ }
